2845f69ee6687d99ea0b916ec44514be11dafedd
Victor Salvaya <victorsalvaya@hotmail.com>
2020-06-29 18:49:26 -0300

    Updates

---
+++ precache-manifest.44cd6b4bcdc2c8415e4c11979533b4e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7746d512747b9ba65be6d731066ba80",
+    "url": "/Estoque-goodwill-electronics/index.html"
+  },
+  {
+    "revision": "e13fa9065e23647198cb",
+    "url": "/Estoque-goodwill-electronics/static/css/main.0c8f6a87.chunk.css"
+  },
+  {
+    "revision": "1c4b977426f3ec18e39d",
+    "url": "/Estoque-goodwill-electronics/static/js/2.8b30512c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Estoque-goodwill-electronics/static/js/2.8b30512c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13fa9065e23647198cb",
+    "url": "/Estoque-goodwill-electronics/static/js/main.bb08e9be.chunk.js"
+  },
+  {
+    "revision": "efc8e8b17af091e54f13",
+    "url": "/Estoque-goodwill-electronics/static/js/runtime-main.2c376d6b.js"
+  },
+  {
+    "revision": "2e85e050ad3dbeaf3918f4e8234b4abf",
+    "url": "/Estoque-goodwill-electronics/static/media/logo-empresa.2e85e050.png"
+  }
+]);
